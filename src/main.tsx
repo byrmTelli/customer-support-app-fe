cfe1eb29f78473ef282d25dc2baec40878c44381
byrmTelli <byrm.telli.contact@gmail.com>
2024-10-17 14:10:10 +0300

    implement redux toolkit store

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { store } from "./store/store.ts";
+import { Provider as StoreProvider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <StoreProvider store={store}>
+        <App />
+      </StoreProvider>
     </BrowserRouter>
   </StrictMode>
 );
